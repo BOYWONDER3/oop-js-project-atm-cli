a42c99e180cd7054f5776c36cb4355c3c349e6a8
boywonder3 <opeyemiisaacboywonder@gmail.com>
2023-02-13 20:04:45 +0100

    second commit

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 // 1. Ask for account
 // 2. If account does not exist, ask to create account
+// 3. Ask what they want to do
+// 4. Execute command
+//  a. View
+//  b. Withdraw
+//  c. Deposit
+
+// Account
+const Account = require('./Account')
+const CommandLine = require('./CommandLine')
+
+async function main() {
+
+    const accountName = await CommandLine.ask(
+        'Which account would like to access?'
+        )
+        const account = Account.find(accountName)
+}
+
+main()
