5b26db1125a28cf8c072c16b49784363f971015d
boywonder3 <opeyemiisaacboywonder@gmail.com>
2023-02-14 06:09:03 +0100

    third commit

--- Account.js
+++ Account.js
@@ -1,3 +1,36 @@
+const FileSystem = require('./FileSystem')
 module.exports = class Account{
-    static find(accountName) {}
+    constructor(name) {
+        this.#name = name
+    }
+
+    #name
+    #balance
+
+get name() {
+    return this.#name
+}
+
+get balance() {
+    return this.#balance
+}
+get filePath() {
+    return 'accounts/${this.name}.txt'
+}
+
+#load() {
+    return new Promise((resolve, reject) => {
+        fs.readFile(this.filePath, (err, data) => {
+            if (err) return reject(err)
+            this.#balance = parseFloat(data)
+            resolve
+        })
+    })
+
+}
+    async static find(accountName) {
+        const account = new Account(accountName)
+
+        await account.load()
+    }
 }
